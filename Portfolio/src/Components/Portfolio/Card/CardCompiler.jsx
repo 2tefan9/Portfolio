e062523d1da14eb45d098ebd5a55c6e2af5fe304
2tefan9 <stef2n9@gmail.com>
2024-11-15 13:06:37 +0100

    clear

--- Portfolio/src/Components/Portfolio/Card/CardCompiler.jsx
+++
@@ -1,8 +0,0 @@
-const CardCompiler = [
-    { id: 1, img: "/src/assets/img/NumberRainGif.svg",hoverImg:"/src/assets/Gif/NumberRainGif.gif", nome: "Math Rain (2024)", descrizione: "First project developed in Unity Engine" },
-    { id: 2, img: "/src/assets/img/DumbyDumblings.svg",hoverImg:"/src/assets/Gif/DumbyDumblings.gif", nome: "Dumby Dumblings (2024)", descrizione: "First team project in Unity Engine" },
-    { id: 3, img: "/src/assets/img/ShatteredPeak.svg", hoverImg:"/src/assets/Gif/ShatteredPeak.gif",nome: "The Shattered Peak (2024)", descrizione: "First team project in Unreal Engine" },
-    // Aggiungi altri progetti...
-];
-
-export default CardCompiler;
