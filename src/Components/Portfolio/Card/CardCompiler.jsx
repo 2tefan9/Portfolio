2b4b71f2d1c775bf0c783911a5dc6435faff8bb3
2tefan9 <stef2n9@gmail.com>
2024-11-16 01:14:19 +0100

    a

--- src/Components/Portfolio/Card/CardCompiler.jsx
+++ src/Components/Portfolio/Card/CardCompiler.jsx
@@ -1,6 +1,6 @@
 const CardCompiler = [
     { id: 1, img: "tree/gh-pages/img/NumberRainGif.svg",hoverImg:"/img/DumbyDumblings.svg", nome: "Math Rain (2024)", descrizione: "First project developed in Unity Engine" },
-    { id: 2, img: "tree/gh-pages/img/DumbyDumblings.svg",hoverImg:"tree/gh-pages/Gif/DumbyDumblings.gif", nome: "Dumby Dumblings (2024)", descrizione: "First team project in Unity Engine" },
+    { id: 2, img: "tree/gh-pages/img/DumbyDumblings.svg",hoverImg:"/Gif/DumbyDumblings.svg", nome: "Dumby Dumblings (2024)", descrizione: "First team project in Unity Engine" },
     { id: 3, img: "tree/gh-pages/img/ShatteredPeak.svg", hoverImg:"tree/gh-pages/Gif/ShatteredPeak.gif",nome: "The Shattered Peak (2024)", descrizione: "First team project in Unreal Engine" },
     // Aggiungi altri progetti...
 ];
